210f1c13ab53ce1e8deadc63fcb1a6540b616714
iNikitaGricenko <VfVristalt@gmail.com>
2022-08-03 10:48:58 +0300

    fix applescript

--- src/Processes.java
+++ src/Processes.java
@@ -1,4 +1,5 @@
 import com.sun.jna.Native;
+import com.sun.jna.Platform;
 import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.Psapi;
 import com.sun.jna.platform.win32.User32;
@@ -12,25 +13,25 @@ import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+import static com.sun.jna.Platform.*;
 import static com.sun.jna.platform.win32.WinDef.HWND;
 import static com.sun.jna.platform.win32.WinNT.PROCESS_QUERY_INFORMATION;
 import static com.sun.jna.platform.win32.WinNT.PROCESS_VM_READ;
 import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 
 public class Processes {
 
 	private static final int BUFFER_SIZE = 1024;
 	private static final String SCRIPT = "global frontApp, frontAppName, windowTitle\n" +
 			"\n" +
+			"set windowTitle to \"\"\n" +
 			"tell application \"System Events\"\n" +
 			"\tset frontApp to first application process whose frontmost is true\n" +
 			"\tset frontAppName to name of frontApp\n" +
@@ -59,25 +60,26 @@ public class Processes {
 	public Application getOpenedApplicationNames() {
 		String systemOS = System.getProperty("os.name").toLowerCase();
 
-		if (systemOS.contains("linux")) {
-			macOsRunningProcesses();
-//			return linuxRunningProcesses();
-		} else if (systemOS.contains("win")) {
+		if (isLinux()) {
+			return linuxRunningProcesses();
+		} else if (isWindows()) {
 			return getWindowsActiveWindowInfo();
-		} else if (systemOS.contains("mac")) {
+		} else if (isMac()) {
 			macOsRunningProcesses();
 		}
+
+		System.out.println(getOSType());
 
 		throw new RuntimeException(format("OS %s is unsupported", systemOS));
 	}
 
-	private static void macOsRunningProcesses() throws ScriptException {
-		Optional<ScriptEngine> appleScript = Optional.ofNullable(new ScriptEngineManager().getEngineByName("AppleScript"));
-		appleScript.ifPresent(Processes::extractApplicationTitle);
+	private void macOsRunningProcesses() throws ScriptException {
+		Optional<ScriptEngine> appleScript = ofNullable(new ScriptEngineManager().getEngineByName("AppleScript"));
+		appleScript.ifPresent(this::extractApplicationTitle);
 	}
 
 	@SneakyThrows
-	private static void extractApplicationTitle(ScriptEngine appleScript) {
+	private void extractApplicationTitle(ScriptEngine appleScript) {
 		String result=(String) appleScript.eval(SCRIPT);
 		System.out.println(result);
 	}
@@ -93,11 +95,13 @@ public class Processes {
 	public Application getWindowsActiveWindowInfo() {
 		String appProcess = executeWindCommand((buffer, hwnd) -> {
 			WinNT.HANDLE handle = getKernelHandle();
-			return getPsapi().GetModuleFileNameExW(handle, null, buffer, BUFFER_SIZE);
+			return getPsapi().GetModuleFileNameExW(handle, null, buffer, BUFFER_SIZE); // returns app title in example RunningProcesses – Processes.java https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getwindowmodulefilenamea
 		}); // returns app name like path, in example C:\Program Files\JetBrains\IntelliJ IDEA Ultimate\bin\idea64.exe
 
+		// getPsapi().GetModuleBaseName https://stackoverflow.com/questions/17061090/how-to-get-process-information-from-windowhandle-in-native-with-java https://docs.microsoft.com/en-us/windows/win32/psapi/enumerating-all-processes
+
 		String appTitle = executeWindCommand((buffer, hwnd) ->
-				getUser32().GetWindowText(hwnd, buffer, BUFFER_SIZE)); // returns app title in example RunningProcesses – Processes.java
+				getUser32().GetWindowText(hwnd, buffer, BUFFER_SIZE));
 
 		return new Application(appProcess, appTitle);
 	}
